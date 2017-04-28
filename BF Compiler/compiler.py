91977e621f0c6b44f06ef457bf10d7ed391f25ce
Zomarea <acasterkian@gmail.com>
2017-04-28 12:55:06 +0100

    Update compiler.py

--- BF Compiler/compiler.py
+++ BF Compiler/compiler.py
@@ -4,17 +4,24 @@ cells = [] # The cell array
 pointer = 0 # The index pointer for the cell array
 instructions = [] # An array of each instruction from the code file
 string = [] # An array of characters from each cell's ASCII value (if printed)
-final_string = ""
+final_string = "" # A string of every character from the string array (joined)
 
+# A loop to predefine each index in the cell array (between 0 and the limit) to be 0
 for i in range(limit): 
     cells.append(0) 
+# A function to traverse through the file loaded to the compiler and push each individual character to the instructions array    
 def parse_instructions(inst, arr):
     for i in range(len(inst)):
         arr.append(inst[i])
+# Loads the file into the compiler        
 file = input()
 instruction = open(file,"r")
+# Allows the user to type in specific instructions to the compiler (right now only "compile")
 console_instruction = input()
+# Runs the function "parse_instructions" 
 parse_instructions(instruction.read(), instructions)
+
+# This only happens if the user types "compile" into the console, this will run the BF program
 if console_instruction == "compile":
     for i in range(len(instructions)):
         if instructions[i] == "+": 
